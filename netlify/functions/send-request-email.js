a6806db9965797eb491863a22236412c916a6884
Luca Morandini MBPro <lucamorandinimbpro@lucas-mbp.home>
2026-08-27 17:05:50 +0200

    Forza un nuovo deploy Netlify della funzione email
    Il deploy precedente non sembra aver aggiornato la funzione live
    (verificato: non applica la nuova validazione per kind=proposta).

--- netlify/functions/send-request-email.js
+++ netlify/functions/send-request-email.js
@@ -1,4 +1,5 @@
 const nodemailer = require('nodemailer');
+// build-marker: 20260827-1710
 
 function esc(s) {
   return String(s || '').replace(/[&<>"']/g, (c) => ({ '&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;', "'": '&#39;' }[c]));
